1a06d84e2dfa7e3c330fff55c114f02f2e27e237
alireza00027 <alirezaghotbadini@gmail.com>
2021-10-27 12:40:02 +0330

    Revert "f1 test"
    This reverts commit 61d44bfb99cdf93b837a6d0947918ec30704faf8.

--- app/Http/Controllers/Admin/ExamController.php
+++ app/Http/Controllers/Admin/ExamController.php
@@ -9,27 +9,30 @@ use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 
-class ExamController extends Controller {
+class ExamController extends Controller
+{
 
-    public function index() {
-        $title = "لیست امتحانات 1";
-        $exams = Exam::filter()->with('user')->with('course')->latest()->paginate(30);
-        return view('admin.exams.index', compact('title', 'exams'));
+    public function index()
+    {
+        $title="لیست امتحانات";
+        $exams=Exam::filter()->with('user')->with('course')->latest()->paginate(30);
+        return view('admin.exams.index',compact('title','exams'));
     }
-
-    public function chartStyle() {
-        $title = 'تحلیل نموداری';
-        $exams = Exam::selectRaw('PDATE(created_at) day , point')
+    
+    public function chartStyle()
+    {
+        $title='تحلیل نموداری';
+        $exams=Exam::selectRaw('PDATE(created_at) day , point')
             ->filter()
-            ->where('created_at', '>', Carbon::now()->subMonth(6))
+            ->where('created_at','>',Carbon::now()->subMonth(6))
             ->orderBy('created_at', 'desc')
             ->get();
-        return view('admin.exams.chart-style', compact('title', 'exams'));
+        return view('admin.exams.chart-style',compact('title','exams'));
     }
-
+    
     public function create() {
-        $title = "ثبت آزمون صبحانه";
-        return view('admin.exams.create', compact('title'));
+        $title="ثبت آزمون صبحانه";
+        return view('admin.exams.create',compact('title'));
     }
 
     public function store(Request $request) {
